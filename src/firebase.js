80abfc32309207341f509390f3b287d4bc208b35
bahaa-dot <bahaa@qahwaclub.com>
2025-10-05 13:40:20 +0400

    firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCzySVIeOmd1dpGxRj0D__tk8zDNBj0yJc",
   authDomain: "wealthtracker-a2600.firebaseapp.com",
@@ -12,8 +11,6 @@ const firebaseConfig = {
   appId: "1:745752113389:web:be09c6c0d70afc098d1ce4"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
 export const db = getFirestore(app);
+export const auth = getAuth(app);
